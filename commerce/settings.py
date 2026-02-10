e47846ccb191880214ec96477caf380a8c51006d
Cheshta-Agarwal <agarwalcheshta2050@gmail.com>
2026-02-10 12:19:10 +0530

    Second commit

--- commerce/settings.py
+++ commerce/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k$81#f48$xql&@3*0iwva(4#redapvf=5n#ljutbn3jjtf$imv'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
